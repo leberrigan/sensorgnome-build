a70e7a02b9a1985059dd2283664c25e62d8ea36e
Lucas Berrigan <leberrigan@gmail.com>
2026-05-01 12:12:14 -0300

    update promote-packages.sh

--- promote-packages.sh
+++ promote-packages.sh
@@ -10,9 +10,9 @@
 #     https://sensorgnome.s3.us-east-2.amazonaws.com/dists/stable/main/binary-armhf/Packages
 # Bookworm uses booktest and bookworm
 wget -q -O Packages-booktest \
-    https://sensorgnome.s3.us-east-2.amazonaws.com/dists/booktest/main/binary-armhf/Packages
+    https://sensorgnome-982081078525-us-east-1-an.s3.us-east-1.amazonaws.com/dists/booktest/main/binary-armhf/Packages
 wget -q -O Packages-bookworm \
-    https://sensorgnome.s3.us-east-2.amazonaws.com/dists/bookworm/main/binary-armhf/Packages
+    https://sensorgnome-982081078525-us-east-1-an.s3.us-east-1.amazonaws.com/dists/bookworm/main/binary-armhf/Packages
 
 #deps=$(awk -v ORS= '/^Depends:/,/[^,]$/' DEBIAN/control | sed -e 's/^\S*://' -e 's/ *, */ /g')
 deps=$(egrep '^Package' Packages-booktest | sed -e 's/^\S*:\s*//' | sort -u)
@@ -22,9 +22,17 @@ for d in $deps; do
     version_s=$(awk '/^Package:/{pkg=$2} /^Version/&&pkg=="'$d'"{print $2}' Packages-bookworm | sort | tail -1)
     echo "$d -- testing: $version_t, stable: $version_s"
     if [[ x$version_t != $version_s ]]; then
-        out=$(deb-s3 copy --bucket=sensorgnome --s3-region=$S3_REGION --preserve-versions \
-            --codename=booktest --sign=8EFF151A5DDAE8F1 --visibility=public --arch=armhf \
-            $d bookworm main --versions=$version_t)
+        out=$(deb-s3 copy \
+        --bucket=sensorgnome-982081078525-us-east-1-an \
+        --s3-region="$S3_REGION" \
+        --preserve-versions \
+        --sign=1DC0C66E4594570A \
+        --arch=armhf \
+        --codename=booktest \
+        --component=main \
+        "$d" bookworm main \
+        --versions="$version_t"
+        )
         if [[ "$out" =~ "ERROR" ]]; then exit 1; fi
     fi
 done
